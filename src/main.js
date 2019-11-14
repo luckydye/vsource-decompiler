743b13fd1b563ca0517bc5a16ce7eeb0906a0af6
Tim Havlicek <tim.h4vlicek@gmail.com>
2019-11-15 00:37:55 +0100

    output file read speed

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import BSPFile from './BSPFile.js';
 
 async function start() {
     const req = await fetch('../res/ar_shoots.bsp');
+
+    const startParse = performance.now();
+
     const bsp = new BSPFile(await req.arrayBuffer());
 
     console.log(bsp);
+    console.log('File read in', performance.now() - startParse, 'ms');
 }
 
 start();
