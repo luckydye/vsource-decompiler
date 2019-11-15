f02a4b05aab1ea98b2bed6b8ed05ddd127f210e1
Tim Havlicek <tim.havlicek@chamaeleon.de>
2019-11-15 12:37:43 +0100

    2d mapping

--- src/BSPFile.js
+++ src/BSPFile.js
@@ -14,7 +14,25 @@ function bytesToInteger(bytes) {
 }
 
 function bytesToFloat(bytes) {
-    return (bytes[0] << 24) | (bytes[1] << 16) | (bytes[2] << 8) | bytes[3];
+    return Bytes2Float32(bytesToInteger(bytes));   
+}
+
+// https://stackoverflow.com/questions/4414077/read-write-bytes-of-float-in-js
+function Bytes2Float32(bytes) {
+    var sign = (bytes & 0x80000000) ? -1 : 1;
+    var exponent = ((bytes >> 23) & 0xFF) - 127;
+    var significand = (bytes & ~(-1 << 23));
+
+    if (exponent == 128) 
+        return sign * ((significand) ? Number.NaN : Number.POSITIVE_INFINITY);
+
+    if (exponent == -127) {
+        if (significand == 0) return sign * 0.0;
+        exponent = -126;
+        significand /= (1 << 22);
+    } else significand = (significand | (1 << 23)) / (1 << 23);
+
+    return sign * significand * Math.pow(2, exponent);
 }
 
 function bytesToShort(bytes) {
--- src/main.js
+++ src/main.js
@@ -11,6 +11,38 @@ async function start() {
 
     console.log(bsp);
     console.log('File read in', performance.now() - startParse, 'ms');
+
+    draw2dMap(bsp);
+}
+
+function draw2dMap(bsp) {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+
+    canvas.width = 400;
+    canvas.height = 400;
+
+    const verts = bsp.vertecies;
+
+    ctx.fillStyle = "#eee";
+
+    const c = [
+        canvas.width / 2,
+        canvas.height / 2,
+    ];
+
+    const s = 20;
+
+    for(let vert of verts) {
+        ctx.fillRect(
+            c[0] + vert.x * s, 
+            c[1] + vert.y * s, 
+            1, 
+            1
+        );
+    }
+
+    document.body.appendChild(canvas);
 }
 
 start();
