a7fe6cc6fa1285a71e4d2e4f5e99e274658fabe4
Tim Havlicek <tim.h4vlicek@gmail.com>
2019-11-23 17:33:54 +0100

    module index

---
+++ index.js
@@ -0,0 +1,15 @@
+import VVDFile from './src/VVDFile.js';
+import BSPFile from './src/BSPFile.js';
+import VPKFile from './src/VPKFile.js';
+import MDLFile from './src/MDLFile.js';
+import VMTFile from './src/VMTFile.js';
+import VTFFile from './src/VTFFile.js';
+
+export {
+    VVDFile,
+    BSPFile,
+    VPKFile,
+    MDLFile,
+    VMTFile,
+    VTFFile,
+}
