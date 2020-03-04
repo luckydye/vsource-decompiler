fda316012a4e199f0d908c9764e0690a773ce489
Tim Havlicek <tim.h4vlicek@gmail.com>
2020-03-04 02:19:00 +0100

    glb spec

--- files/GLBFile.mjs
+++ files/GLBFile.mjs
@@ -1,16 +1,44 @@
 import { BinaryFile } from "./BinaryFile.mjs";
 
-export default class GLTFFile extends BinaryFile {
+export default class GLBFile extends BinaryFile {
 
     /*  Format
     
 
     */
 
-    constructor(name) {
+    constructor(geometry) {
         super();
         
-        this.name = name;
+        this.gltf = GLTFFile.fromGeometry(geometry);
+
+        const fileLength = 0;
+
+        const header = {
+            magic: { 'unsigned int': 'glTF' },
+            version: { 'unsigned int': 2 },
+            length: { 'unsigned int': fileLength },
+        }
+
+        const chunkLength = 0;
+
+        const JSON = 0x4E4F534A;
+        const BIN = 0x004E4942;
+
+        const chunkData = new Float32Array();
+
+        // JSON chunk:
+        // encode:
+        // new TextEncoder().encode(JSON.stringify({ asd: 123 }))
+
+        // decode: 
+        // JSON.parse(new TextDecoder().decode(Uint8Array))
+
+        const chunk = {
+            chunkLength: { 'unsigned int': chunkLength },
+            chunkType: { 'unsigned int': 'JSON' }, // or 'BIN'
+            chunkData: { 'binary': chunkData },
+        }
     }
 
 }
