21405a19ef227611641c8f2a6d96aadd43d46a75
luckydye <tim.h4vlicek@gmail.com>
2020-03-06 23:50:51 +0100

    ported FBX file from gyro

---
+++ files/FBXFile.mjs
@@ -0,0 +1,94 @@
+import { BinaryFile } from "./BinaryFile.mjs";
+
+const Structs = {
+    header: {
+        magic: 'unsigned char',
+        unknown: 'byte[2]',
+        version: 'unsigned int',
+    },
+    nodeRecord: {
+        endOffset: 'int',
+        numProperties: 'int',
+        propertyListLen: 'int',
+        nameLen: 'byte',
+        name: 'char[nameLen]',
+        
+        propertyData: 'byte[propertyListLen]',
+        // ... properties || Property[n], for n in 0:PropertyListLen
+
+    },
+    propertyRecord: {
+        typeCode: 'char[1]',
+    },
+}
+
+export default class FBXFile extends BinaryFile {
+
+    static get STRUCT() {
+        return Structs;
+    }
+
+    static fromDataArray(dataArray) {
+        const fbx = this.createFile(dataArray);
+
+        const header = this.unserialize(fbx.view, 0, Structs.header);
+
+        fbx.header = header.data;
+        fbx.nodes = {};
+
+        if(fbx.header.magic.data !== "Kaydara FBX Binary  ") {
+            throw new Error('File type not recognised');
+        }
+
+        let byteOffset = header.byteOffset;
+
+        const unserializeNode = (nodeEndOffset) => {
+
+            const node = this.unserialize(fbx.view, byteOffset, Structs.nodeRecord);
+            byteOffset = node.byteOffset;
+
+            const nodeData = node.data;
+            nodeData.children = [];
+
+            nodeEndOffset = nodeEndOffset || nodeData.endOffset.data;
+
+            // node scope start
+            while(byteOffset < nodeEndOffset) {
+                // resolve nodes in current scope
+                const childNode = unserializeNode(nodeData.endOffset.data);
+                
+                if(childNode.endOffset.data == 0) {
+                    // close scope if node is a NULL terminator
+                    return nodeData;
+                } else {
+                    nodeData.children.push(childNode);
+                }
+            }
+
+            return nodeData;
+        }
+
+        while(byteOffset < fbx.view.byteLength - 13) {
+            try {
+                const node = unserializeNode();
+
+                if (node.endOffset.data < fbx.view.byteLength && 
+                    node.endOffset.data != 0) {
+                        
+                    fbx.nodes[node.name.data] = node;
+                }
+
+            } catch(err) {
+                break;
+                console.warn(err);
+            }
+        }
+
+        return fbx;
+    }
+
+    convertToMesh() {
+        
+    }
+
+}
