c715a234e062806143551ccbeaed340b08df60ce
Tim Havlicek <tim.h4vlicek@gmail.com>
2020-03-23 19:13:10 +0100

    read vpk directory

--- files/VPKFile.mjs
+++ files/VPKFile.mjs
@@ -1,23 +1,76 @@
 import { BinaryFile } from 'binary-file-lib';
+import { VPK } from './VPKStructure.mjs';
 
 // https://developer.valvesoftware.com/wiki/VPK_File_Format#Conception
 
-const VPKHeader_v2 = {
-    TreeSize: 'unsigned int',
-    FileDataSectionSize: 'unsigned int',
-    ArchiveMD5SectionSize: 'unsigned int',
-    OtherMD5SectionSize: 'unsigned int',
-    SignatureSectionSize: 'unsigned int',
-}
-
-const Structs = {
-    VPKHeader_v2,
-}
-
 export default class VPKFile extends BinaryFile {
 
-    static get STRUCT() {
-        return Structs;
+    static deserializeNodeTree(vpk, offset) {
+
+        const files = {};
+
+        let fileType;
+        let filePath;
+        let fileName;
+
+        const readString = () => {
+            const string = this.parseType(vpk.view, offset, 'unsigned char');
+            offset = string.byteOffset;
+
+            return string.valueOf() == "" ? null : string;
+        }
+
+        const readFile = () => {
+            const fileData = this.unserialize(vpk.view, fileName.byteOffset, VPK.VPKDirectoryEntry);
+
+            const preloadBytes = fileData.data.PreloadBytes.valueOf();
+            const followsDirectory = fileData.data.ArchiveIndex.valueOf() === 0x7fff;
+
+            if(fileData.data.Terminator.valueOf() !== 0xffff) {
+                throw new Error('Failed parsing VPK.');
+            }
+
+            const entryOffset = fileData.data.EntryOffset.valueOf();
+            const entryLength = fileData.data.EntryLength.valueOf();
+
+            offset = fileData.byteOffset + preloadBytes;
+
+            const filePathName = filePath.valueOf() + "/" + 
+                                 fileName.valueOf() + "." +
+                                 fileType.valueOf();
+
+            files[filePathName] = {
+                CRC: fileData.data.CRC.valueOf(),
+                PreloadBytes: fileData.data.PreloadBytes.valueOf(),
+                ArchiveIndex: fileData.data.ArchiveIndex.valueOf(),
+                EntryOffset: fileData.data.EntryOffset.valueOf(),
+                EntryLength: fileData.data.EntryLength.valueOf(),
+                Terminator: fileData.data.Terminator.valueOf(),
+            };
+
+            return true;
+        }
+
+        while(true) {
+            fileType = readString();
+            if(!fileType) break;
+
+            while(true) {
+                filePath = readString();
+                if(!filePath) break;
+
+                while(true) {
+                    fileName = readString();
+                    if(fileName) {
+                        readFile();
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return files;
     }
 
     static fromDataArray(dataArray) {
@@ -26,9 +79,11 @@ export default class VPKFile extends BinaryFile {
         vpk.buffer = dataArray;
         vpk.view = new DataView(dataArray);
 
-        vpk.header = this.unserialize(vpk.view, 0, Structs.VPKHeader_v2);
+        vpk.header = this.unserialize(vpk.view, 0, VPK.VPKHeader_v2);
 
-        console.log(vpk.header);
+        // const dataOffset = vpk.header.byteOffset + vpk.header.data.TreeSize.valueOf();
+
+        vpk.files = this.deserializeNodeTree(vpk, vpk.header.byteOffset);
 
         return vpk;
     }
---
+++ files/VPKStructure.mjs
@@ -0,0 +1,43 @@
+export const VPK =  {
+
+    VPKHeader_v2: {
+        Signature: 'unsigned int',
+        Version: 'unsigned int',
+        TreeSize: 'unsigned int',
+        FileDataSectionSize: 'unsigned int',
+        ArchiveMD5SectionSize: 'unsigned int',
+        OtherMD5SectionSize: 'unsigned int',
+        SignatureSectionSize: 'unsigned int',
+    },
+    
+    VPKDirectoryEntry: {
+        CRC: 'unsigned int',
+        PreloadBytes: 'unsigned short',
+        ArchiveIndex: 'unsigned short',
+        EntryOffset: 'unsigned int',
+        EntryLength: 'unsigned int',
+        Terminator: 'unsigned short',
+    },
+    
+    VPK_ArchiveMD5SectionEntry: {
+        ArchiveIndex: 'unsigned int',
+        StartingOffset: 'unsigned int',
+        Count: 'unsigned int',
+        MD5Checksum: 'char[16]',
+    },
+    
+    VPK_OtherMD5Section: {
+        TreeChecksum: 'char[16]',
+        ArchiveMD5SectionChecksum: 'char[16]',
+        Unknown: 'char[16]',
+    },
+    
+    VPK_SignatureSection: {
+        PublicKeySize: 'unsigned int',
+        PublicKey: 'char[PublicKeySize]',
+
+        SignatureSize: 'unsigned int',
+        Signature: 'char[SignatureSize]',
+    },
+
+}
